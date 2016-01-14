960c93af91a9139285476c99e3b510561f7750bf
willkewley <william.kewley@kbbs.ie>
2016-01-14 12:03:56 +0000

    This Commit solves the issue in Jira NMAPP-439

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Table_1]
 (
 [Col1] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
-[Col2] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+[Col2] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+[Col3] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
