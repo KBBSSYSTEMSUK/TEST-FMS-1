2862fff88920aa02e944ab567611fc04814b6ff6
willkewley <william.kewley@kbbs.ie>
2016-01-14 12:58:53 +0000

    Revert "Added Col5 to solve the JIRA issue NMAPP-440"
    This reverts commit 72af2faffe266afb0c08346fb224b3bf6ef83dfd.
    
    # Conflicts:
    #	Tables/dbo.Table_1.sql

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -3,8 +3,12 @@ CREATE TABLE [dbo].[Table_1]
 [Col1] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [Col2] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [Col3] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
+<<<<<<< HEAD
 [Col4] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [Col5] [nchar] (10) COLLATE Latin1_General_CI_AS NULL,
 [Col6] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+=======
+[Col4] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
+>>>>>>> parent of 72af2fa... Added Col5 to solve the JIRA issue NMAPP-440
 ) ON [PRIMARY]
 GO
